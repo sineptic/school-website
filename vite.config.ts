81a4727d2136f1f7690a1331d24ab94673bd85f8
sineptic <sineptic0@gmail.com>
2025-11-22 08:55:09 +0300

    splitting bundle by libraries

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,18 @@ export default defineConfig({
     minify: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        manualChunks(id) {
+          // FIXME
+          // NOTE: removing project name, that contains 'preact' in name(filepath)
+          id = id.substring(42);
+          if (id.includes("preact")) {
+            return "preact";
+          }
+          if (id.includes("convex")) {
+            return "convex";
+          }
+          return undefined;
+        },
       },
     },
   },
