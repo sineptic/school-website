a45d788dbbe13ec47f1274e44783ffdb64b28910
sineptic <sineptic0@gmail.com>
2025-11-27 09:57:37 +0300

    restore base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/school-website/",
+  base: "/school-website/",
   plugins: [preact()],
   esbuild: {
     minifyIdentifiers: false,
